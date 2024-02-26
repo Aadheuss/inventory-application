490f0317a5657bd0771f5ed845306042a76a78c5
Aadheuss <aadheusx@gmail.com>
2024-02-26 15:21:09 +0700

    Add database connection

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+// loads environment variables from a .env file into process.env
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set("strictQuery", false);
+// Define the database URL to connect to.
+const mongoDB = process.env.MONGODB_URI;
+// Wait for database to connect, logging an error if there is a problem
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+main().catch((err) => console.log(err));
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
